6b391b59b5553396a1ab9715e28b2c61cbb514eb
Drag-On <drag-on@gmx.de>
2017-08-13 18:31:49 +0200

    Provide functionality in SceneTab to open a scene

--- core/include/novelist/widgets/SceneTabWidget.h
+++ core/include/novelist/widgets/SceneTabWidget.h
@@ -11,6 +11,8 @@
 
 #include <QtWidgets/QTabWidget>
 #include <QtCore/QFile>
+#include "model/ProjectModel.h"
+#include "TextEditor.h"
 
 namespace novelist {
     class SceneTabWidget : public QTabWidget {
@@ -18,8 +20,38 @@ namespace novelist {
 
     public:
         explicit SceneTabWidget(QWidget* parent = nullptr);
-        
+
+        /**
+         * If the scene is not open yet, loads it up from hard disk and creates a new tab for it. Makes the tab of the
+         * scene active.
+         * @param model Pointer to the model
+         * @param index Index of a scene node of the model
+         */
+        void openScene(ProjectModel* model, QModelIndex index);
+
+        /**
+         * @param model Pointer to the model
+         * @param index Index of a scene node of the model
+         * @return Index of the page with the editor for the requested scene, or -1 if it cannot be found
+         */
+        int indexOf(ProjectModel const* model, QModelIndex index) const;
+
+    private slots:
+
+        void onTabCloseRequested(int index);
     };
+
+    namespace internal {
+        class InternalTextEditor : public TextEditor {
+        Q_OBJECT
+
+        public:
+            using TextEditor::TextEditor;
+
+            ProjectModel* m_model;
+            QPersistentModelIndex m_modelIndex;
+        };
+    }
 }
 
 #endif //NOVELIST_SCENETABWIDGET_H
--- core/src/novelist/widgets/SceneTabWidget.cpp
+++ core/src/novelist/widgets/SceneTabWidget.cpp
@@ -7,15 +7,87 @@
  * @details
  **********************************************************/
 
+#include <QTabBar>
+#include <QMessageBox>
 #include "widgets/SceneTabWidget.h"
 
-namespace novelist
-{
+namespace novelist {
     SceneTabWidget::SceneTabWidget(QWidget* parent)
             :QTabWidget(parent)
     {
         setDocumentMode(true);
         setTabsClosable(true);
         setMovable(true);
+        setElideMode(Qt::ElideRight);
+
+        connect(this, &SceneTabWidget::tabCloseRequested, this, &SceneTabWidget::onTabCloseRequested);
+    }
+
+    void SceneTabWidget::openScene(ProjectModel* model, QModelIndex index)
+    {
+        Expects(model->nodeType(index) == ProjectModel::NodeType::Scene);
+
+        int tabIdx = indexOf(model, index);
+        if (tabIdx == -1) {
+            auto* editor = new internal::InternalTextEditor();
+            editor->m_model = model;
+            editor->m_modelIndex = index;
+            editor->setDocument(model->loadScene(index));
+
+            // Make sure the tab title and color change appropriately
+            connect(editor->m_model, &ProjectModel::dataChanged, [this, editor, model] (QModelIndex const& topLeft, QModelIndex const& bottomRight, QVector<int> const& roles)
+            {
+                for (int i = topLeft.row(); i <= bottomRight.row(); ++i) {
+                    QModelIndex curIdx = model->index(i, 0, topLeft.parent());
+                    if (int tabIdx = indexOf(model, curIdx); tabIdx >= 0) {
+                        QString name = model->data(curIdx, Qt::DisplayRole).toString();
+                        tabBar()->setTabText(tabIdx, name);
+                        tabBar()->setTabToolTip(tabIdx, name);
+                        tabBar()->setTabTextColor(tabIdx, qvariant_cast<QBrush>(model->data(curIdx, Qt::ForegroundRole)).color());
+                    }
+                }
+            });
+
+            QString name = model->data(index, Qt::DisplayRole).toString();
+            tabIdx = addTab(editor, name);
+            tabBar()->setTabToolTip(tabIdx, name);
+            tabBar()->setTabTextColor(tabIdx, qvariant_cast<QBrush>(model->data(index, Qt::ForegroundRole)).color());
+        }
+        setCurrentIndex(tabIdx);
+    }
+
+    int SceneTabWidget::indexOf(ProjectModel const* model, QModelIndex index) const
+    {
+        for (int i = 0; i < count(); ++i) {
+            auto* w = dynamic_cast<internal::InternalTextEditor*>(widget(i));
+            if (w != nullptr && w->m_model == model && w->m_modelIndex == index)
+                return i;
+        }
+
+        return -1;
+    }
+
+    void SceneTabWidget::onTabCloseRequested(int index)
+    {
+        auto* w = dynamic_cast<internal::InternalTextEditor*>(widget(index));
+        if (w != nullptr && w->document()->isModified()) {
+            QMessageBox msgBox;
+            msgBox.setText(tr("The document has been modified."));
+            msgBox.setInformativeText(
+                    tr("If you close it now, all changes will be lost. Are you sure you want to continue?"));
+            msgBox.setStandardButtons(QMessageBox::Discard | QMessageBox::Cancel);
+            msgBox.setDefaultButton(QMessageBox::Cancel);
+            msgBox.setIcon(QMessageBox::Question);
+            int ret = msgBox.exec();
+            switch (ret) {
+                case QMessageBox::Discard:
+                    w->m_model->unloadScene(w->m_modelIndex);
+                    break;
+                case QMessageBox::Cancel:
+                default:
+                    return;
+            }
+        }
+        removeTab(index);
     }
 }
