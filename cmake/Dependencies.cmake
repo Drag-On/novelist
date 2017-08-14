14f3951b093b5d4dfe390761e5b708bd47294111
Drag-On <drag-on@gmx.de>
2017-08-14 18:08:36 +0200

    Update Qt

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,7 +2,7 @@
 # ============================================================== #
 # Qt5
 # ============================================================== #
-find_package(Qt5 5.8 REQUIRED COMPONENTS Core Widgets Gui Test Designer PATHS /opt/Qt/5.8/gcc_64/ /home/Qt/5.8/gcc_64/)
+find_package(Qt5 5.9 REQUIRED COMPONENTS Core Widgets Gui Test Designer PATHS /opt/Qt/5.9.1/gcc_64/ /home/Qt/5.9.1/gcc_64/)
 set(CMAKE_INCLUDE_CURRENT_DIR ON)   # Find includes in corresponding build directories
 set(CMAKE_AUTOMOC ON)               # Instruct CMake to run moc automatically when needed.
 set(CMAKE_AUTORCC ON)               # Instruct CMake to run rcc automatically when needed.
