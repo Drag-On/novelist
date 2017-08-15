2ba86abe12580447f7968399a658d187151fdace
Drag-On <drag-on@gmx.de>
2017-08-15 14:29:15 +0200

    Improve Id unit tests

--- core/test/identity/IdentityTest.cpp
+++ core/test/identity/IdentityTest.cpp
@@ -31,6 +31,23 @@ bool checkIdsUnique(std::vector<IdType> const& ids)
     return true;
 }
 
+bool checkIdsValid(std::vector<IdType> const& ids)
+{
+    for(auto const& id : ids) {
+        if(!internal::isValidId(id))
+            return false;
+    }
+    return true;
+}
+
+void printIds(std::vector<IdType> const& ids)
+{
+    for(auto const& id : ids) {
+        std::cout << id << ", ";
+    }
+    std::cout << std::endl;
+}
+
 TEST_CASE("Id copy & move constraints", "[Identity]")
 {
     REQUIRE_FALSE(std::is_copy_assignable_v<IdMgr>);
@@ -53,6 +70,7 @@ TEST_CASE("Id generation", "[Identity]")
         ids.emplace_back(manager.generate());
 
     REQUIRE(checkIdsUnique(ids));
+    REQUIRE(checkIdsValid(ids));
 
     static std::random_device rd;
     static std::mt19937 gen(rd());
@@ -62,11 +80,13 @@ TEST_CASE("Id generation", "[Identity]")
     }
 
     REQUIRE(checkIdsUnique(ids));
+    REQUIRE(checkIdsValid(ids));
 
     for(int i = 0; i < 50; ++i)
         ids.emplace_back(manager.generate());
 
     REQUIRE(checkIdsUnique(ids));
+    REQUIRE(checkIdsValid(ids));
 }
 
 TEST_CASE("Id request", "[Identity]")
@@ -78,13 +98,26 @@ TEST_CASE("Id request", "[Identity]")
         ids.emplace_back(manager.generate());
 
     REQUIRE_NOTHROW(manager.request(60));
+    REQUIRE(manager.request(60).id() == 60);
 
     REQUIRE_THROWS(manager.request(0));
 
     REQUIRE(checkIdsUnique(ids));
+    REQUIRE(checkIdsValid(ids));
 
     for(int i = 0; i < 50; ++i)
         ids.emplace_back(manager.generate());
 
     REQUIRE(checkIdsUnique(ids));
+    REQUIRE(checkIdsValid(ids));
+
+    ids.erase(ids.begin(), ids.begin() + 10);
+    REQUIRE(checkIdsUnique(ids));
+    REQUIRE(checkIdsValid(ids));
+
+    for(int i = 0; i < 10; ++i)
+        REQUIRE_NOTHROW(ids.emplace_back(manager.request(i)));
+
+    REQUIRE(checkIdsUnique(ids));
+    REQUIRE(checkIdsValid(ids));
 }
