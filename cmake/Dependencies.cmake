3efd44ba931bc51c28f91eb78ba481723ccc4723
Drag-On <drag-on@gmx.de>
2017-08-19 19:58:19 +0200

    Allow build configuration which doesn't build qt designer libraries

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,11 +2,15 @@
 # ============================================================== #
 # Qt5
 # ============================================================== #
-find_package(Qt5 5.9 REQUIRED COMPONENTS Core Widgets Gui Test Designer PATHS /opt/Qt/5.9.1/gcc_64/ /home/Qt/5.9.1/gcc_64/)
+find_package(Qt5 5.9 REQUIRED COMPONENTS Core Widgets Gui Test PATHS /opt/Qt/5.9.1/gcc_64/ /home/Qt/5.9.1/gcc_64/)
 set(CMAKE_INCLUDE_CURRENT_DIR ON)   # Find includes in corresponding build directories
 set(CMAKE_AUTOMOC ON)               # Instruct CMake to run moc automatically when needed.
 set(CMAKE_AUTORCC ON)               # Instruct CMake to run rcc automatically when needed.
 set(CMAKE_AUTOUIC ON)               # Instruct CMake to run uic automatically when needed.
+
+if (NOT NoDesigner)
+    find_package(Qt5 5.9 REQUIRED COMPONENTS Designer PATHS /opt/Qt/5.9.1/gcc_64/ /home/Qt/5.9.1/gcc_64/)
+endif ()
 
 # ============================================================== #
 # Catch
