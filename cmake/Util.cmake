3bdc769eccb9d086f11a5613a6c724af09e4dea9
Drag-On <drag-on@gmx.de>
2017-09-12 16:40:03 +0200

    Step towards supporting MSVC2017 as compiler

--- cmake/Util.cmake
+++ cmake/Util.cmake
@@ -5,3 +5,26 @@
 function(print_status str)
     message(STATUS ${str})
 endfunction()
+
+# ============================================================== #
+# Enable C++17 for a target
+# ============================================================== #
+function(enable_cxx17 target)
+    # Cmake 3.8 doesn't support this for MSVC, so do it manually
+    # At least Visual Studio 2017 can be supported this way
+    if (MSVC_VERSION GREATER_EQUAL "1900")
+        include(CheckCXXCompilerFlag)
+        CHECK_CXX_COMPILER_FLAG("/std:c++17" _cpp_17_flag_supported)
+        if (_cpp_17_flag_supported)
+            add_compile_options("/std:c++17")
+        endif ()
+    # Try the usual CMake way of adding the standard for other compilers
+    else ()
+        set_target_properties(${target}
+                PROPERTIES
+                CXX_STANDARD 17
+                CXX_STANDARD_REQUIRED YES
+                CXX_EXTENSIONS NO
+                )
+    endif ()
+endfunction()
