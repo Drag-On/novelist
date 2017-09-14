d13cb0f48d10165d91e2e684c0444dd1fd340db2
Drag-On <drag-on@gmx.de>
2017-09-14 18:44:44 +0200

    Add ModelPath class that allows storing arbitrary paths to model indices
    A path is independent of data or even an actual model instance. It just marks a specific node in any tree-structured model. This path stays constant, i.e. if the node in a particular model is moved, a path pointing to this element stays the same.

---
+++ core/include/novelist/model/ModelPath.h
@@ -0,0 +1,137 @@
+/**********************************************************
+ * @file   ModelPath.h
+ * @author jan
+ * @date   9/14/17
+ * ********************************************************
+ * @brief
+ * @details
+ **********************************************************/
+#ifndef NOVELIST_MODELPATH_H
+#define NOVELIST_MODELPATH_H
+
+#include <type_traits>
+#include <initializer_list>
+#include <vector>
+#include <QModelIndex>
+
+namespace novelist {
+    /**
+     * Index of a column and a row in a model
+     */
+    using RowColumnIdx = std::pair<int, int>;
+
+    /**
+     * Path to a node in a model
+     *
+     * This does not have to exist, and neither will it track a node's movement (QPersistentModelIndex does that).
+     * Instead, an object of this type will always point to the same node position, regardless of whether that element
+     * actually exists, has existed before but is now deleted, or if an element on that position has been moved to
+     * another location.
+     */
+    class ModelPath {
+    public:
+        using iterator = decltype(std::declval<std::vector<RowColumnIdx>>().begin());
+        using const_iterator = decltype(std::declval<std::vector<RowColumnIdx>>().cbegin());
+
+        /**
+         * Construct path from a list of rows. Columns are implicitly 0
+         * @param rows List of rows, where every element is interpreted as a child of the previous row. The first entry
+         * is relative to the invisible root element.
+         */
+        ModelPath(std::initializer_list<int> rows) noexcept;
+
+        /**
+         * Construct path from a list of row/column combinations
+         * @param l List of rows and columns, where every element is interpreted as a child of the previous element.
+         * The first entry is relative to the invisible root element.
+         */
+        ModelPath(std::initializer_list<RowColumnIdx> l) noexcept;
+
+        /**
+         * Construct path from a QModelIndex
+         * @param idx Index to construct path from
+         */
+        explicit ModelPath(QModelIndex const& idx) noexcept;
+
+        /**
+         * Construct a QModelIndex based on the path and a model instance. Might be invalid.
+         * @param model Model to construct the QModelIndex for
+         * @return The constructed (possibly invalid) index
+         */
+        QModelIndex toModelIndex(QAbstractItemModel* model) const noexcept;
+
+        /**
+         * Checks whether this path is valid on a model.
+         * @note If you need the actual model index, use toModelIndex() and check the returned index's isValid()
+         * @param model Model to construct the QModelIndex for
+         * @return True if the path is valid on \p model, otherwise false
+         */
+        bool isValid(QAbstractItemModel* model) const noexcept;
+
+        /**
+         * @return Depth of the path
+         */
+        size_t depth() const noexcept;
+
+        /**
+         * @return Iterator to the first element of the path
+         */
+        iterator begin() noexcept;
+
+        /**
+         * @return Iterator to the first element of the path
+         */
+        const_iterator begin() const noexcept;
+
+        /**
+         * @return Iterator to the element following the last element of the path
+         */
+        iterator end() noexcept;
+
+        /**
+         * @return Iterator to the element following the last element of the path
+         */
+        const_iterator end() const noexcept;
+
+        /**
+         * Returns a reference to the path element at specified location.
+         * @param pos Position of the element
+         * @return Reference to requested element
+         */
+        RowColumnIdx& operator[](size_t pos);
+
+        /**
+         * Returns a constant reference to the path element at specified location.
+         * @param pos Position of the element
+         * @return Reference to requested element
+         */
+        RowColumnIdx const& operator[](size_t pos) const;
+
+        /**
+         * Compares two paths for equality
+         * @param other Path to compare to
+         * @return true in case both paths are equivalent, otherwise false
+         */
+        bool operator==(ModelPath const& other) const noexcept;
+
+        /**
+         * Compares two paths for equality
+         * @param other Path to compare to
+         * @return true in case both paths are different, otherwise false
+         */
+        bool operator!=(ModelPath const& other) const noexcept;
+
+        /**
+         * Formatted output
+         * @param stream Stream to use
+         * @param path Path to print
+         * @return The stream
+         */
+        friend std::ostream& operator<<(std::ostream& stream, ModelPath const& path);
+
+    private:
+        std::vector<RowColumnIdx> m_path;
+    };
+}
+
+#endif //NOVELIST_MODELPATH_H
---
+++ core/src/novelist/model/ModelPath.cpp
@@ -0,0 +1,100 @@
+/**********************************************************
+ * @file   ModelPath.cpp
+ * @author jan
+ * @date   9/14/17
+ * ********************************************************
+ * @brief
+ * @details
+ **********************************************************/
+#include "model/ModelPath.h"
+
+namespace novelist {
+    ModelPath::ModelPath(std::initializer_list<int> rows) noexcept
+    {
+        for (int r : rows)
+            m_path.push_back({r, 0});
+    }
+
+    ModelPath::ModelPath(std::initializer_list<RowColumnIdx> l) noexcept
+            :m_path{l}
+    {
+    }
+
+    ModelPath::ModelPath(QModelIndex const& idx) noexcept
+    {
+        QModelIndex iter = idx;
+        while (iter.isValid()) {
+            m_path.insert(m_path.begin(), {iter.row(), iter.column()});
+            iter = iter.parent();
+        }
+    }
+
+    QModelIndex ModelPath::toModelIndex(QAbstractItemModel* model) const noexcept
+    {
+        if (model == nullptr || m_path.empty())
+            return QModelIndex{};
+
+        QModelIndex iter;
+        for (auto const& p : m_path)
+            iter = model->index(p.first, p.second, iter);
+        return iter;
+    }
+
+    bool ModelPath::isValid(QAbstractItemModel* model) const noexcept
+    {
+        return toModelIndex(model).isValid();
+    }
+
+    size_t ModelPath::depth() const noexcept
+    {
+        return m_path.size();
+    }
+
+    ModelPath::iterator ModelPath::begin() noexcept
+    {
+        return m_path.begin();
+    }
+
+    ModelPath::const_iterator ModelPath::begin() const noexcept
+    {
+        return m_path.cbegin();
+    }
+
+    ModelPath::iterator ModelPath::end() noexcept
+    {
+        return m_path.end();
+    }
+
+    ModelPath::const_iterator ModelPath::end() const noexcept
+    {
+        return m_path.cend();
+    }
+
+    RowColumnIdx& ModelPath::operator[](size_t pos)
+    {
+        return m_path[pos];
+    }
+
+    RowColumnIdx const& ModelPath::operator[](size_t pos) const
+    {
+        return m_path[pos];
+    }
+
+    bool ModelPath::operator==(ModelPath const& other) const noexcept
+    {
+        return m_path == other.m_path;
+    }
+
+    bool ModelPath::operator!=(ModelPath const& other) const noexcept
+    {
+        return !(*this == other);
+    }
+
+    std::ostream& operator<<(std::ostream& stream, ModelPath const& path)
+    {
+        stream << "(root)";
+        for(auto [r,c] : path.m_path)
+            stream << "->(" << r << "," << c << ")";
+        return stream;
+    }
+}
