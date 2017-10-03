ed264076c9ceeb90823e29be2c0b854876928ca1
Drag-On <drag-on@gmx.de>
2017-10-03 20:13:07 +0200

    Add macros to simplify data-driven unit testing

---
+++ core/test/macros.h
@@ -0,0 +1,36 @@
+/**********************************************************
+ * @file   macros.h
+ * @author jan
+ * @date   10/3/17
+ * ********************************************************
+ * @brief
+ * @details
+ **********************************************************/
+#ifndef NOVELIST_MACROS_H
+#define NOVELIST_MACROS_H
+
+/**
+ * Define a data-driven section in a catch TEST_CASE
+ */
+#define DATA_SECTION(name, testfun, ...) \
+    SECTION(name) { \
+        auto testFun = testfun; \
+        __VA_ARGS__ \
+    }
+
+/**
+ * Defines the test function to run on all data
+ */
+#define TESTFUN(...) __VA_ARGS__
+
+/**
+ * Define a data row and give it a name
+ */
+#define NAMED_ROW(name, ...) SECTION(name) { testFun(__VA_ARGS__); }
+
+/**
+ * Define a data row without a name
+ */
+#define ROW(...) NAMED_ROW(#__VA_ARGS__, __VA_ARGS__)
+
+#endif //NOVELIST_MACROS_H
